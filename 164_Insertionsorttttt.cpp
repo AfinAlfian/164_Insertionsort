a73d05b693ddbe7090e18fda77bf4a29d5e3000b
AfinAlfian <alfinalfiansyah528@gmail.com>
2024-03-14 11:30:45 +0700

    Mendeklarasikan Variabel

--- 164_Insertionsorttttt.cpp
+++ 164_Insertionsorttttt.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int arr[20];															//Membuat Array dengan panjang data 20
-int n;																	//Membuat Variabel inputan n
+int n;	 																//Membuat Variabel inputan n
 
 
 int main() {
